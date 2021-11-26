767e2ef6ed09b15620f4a2407fda4b43c20d4d77
Elisa <elisacanyelles@gmail.com>
2021-11-26 09:00:36 +0100

    added hover in the cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,59 +10,55 @@ const Container = styled.div`
   overflow: hidden;
   box-shadow: 0px 0px 15px -5px;
   border-radius: 3px;
-`
+  :hover {
+    box-shadow: 0px 0px 15px 0px;
+  }
+`;
 const Image = styled.img`
-overflow: hidden;
+  overflow: hidden;
   height: 250px;
-`
+`;
 
 const Date = styled.input`
-margin: 10px 10px;
-`
-const Button = styled.button`
- 
-`
+  margin: 10px 10px;
+`;
+const Button = styled.button``;
 const Input = styled.div`
-display:flex;
-justify-content:flex-start;
-align-items: center;
-`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 const Artist = styled.h3`
-text-align: left;
-padding-left: 15px;
-`
+  text-align: left;
+  padding-left: 15px;
+`;
 const Location = styled.h4`
-text-align: left;
-padding-left: 15px;
-`
-const Cards = ({result}) => {
-
-  
-
- return (
+  text-align: left;
+  padding-left: 15px;
+`;
+const Cards = ({ result }) => {
+  return (
     <Container>
-    
-    <Image src={result.img}/>
-    <form >
-      <Input>
-        <Date
-          type="date"
-          id="start"
-          name="trip-start"
-          value="2021-11-25"
-          min="2021-01-01"
-          max="2021-12-31"
-          readOnly />
-        <Button>Add to favorites</Button>
-      </Input>
-      <Artist>{result.artist}</Artist>
-      <Location>{result.location}</Location>
-        
-     </form>
-  </Container>
-    ) 
- }
+      <Image src={result.img} />
+      <form>
+        <Input>
+          <Date
+            type="date"
+            id="start"
+            name="trip-start"
+            value="2021-11-25"
+            min="2021-01-01"
+            max="2021-12-31"
+            readOnly
+          />
+          <Button>Add to favorites</Button>
+        </Input>
+        <Artist>{result.artist}</Artist>
+        <Location>{result.location}</Location>
+      </form>
+    </Container>
+  );
+};
 
- export default Cards;    
-       
+export default Cards;
